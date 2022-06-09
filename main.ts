397ab06057d9768115afb34eb380ff17628bbb14
Daniel Schmidt <danielmschmidt92@gmail.com>
2022-06-09 10:45:19 +0200

    basic app

--- main.ts
+++ main.ts
@@ -1,14 +1,33 @@
 import { Construct } from "constructs";
-import { App, TerraformStack } from "cdktf";
+import { App, RemoteBackend, TerraformStack } from "cdktf";
+import * as n from "@cdktf/provider-null";
+import * as r from "@cdktf/provider-random";
 
 class MyStack extends TerraformStack {
   constructor(scope: Construct, name: string) {
     super(scope, name);
 
-    // define resources here
+    new n.NullProvider(this, "null");
+    new r.RandomProvider(this, "random");
+
+    const pet = new r.Pet(this, "my-pet");
+    const resource = new n.Resource(this, "hello-world");
+    resource.addOverride("provisioner", [
+      {
+        "local-exec": {
+          command: `echo 'Hello ${pet}'`,
+        },
+      },
+    ]);
   }
 }
 
 const app = new App();
-new MyStack(app, "cdktf-github-actions-terraform-cloud");
+const stack = new MyStack(app, "cdktf-github-actions-terraform-cloud");
+new RemoteBackend(stack, {
+  organization: "cdktf-team",
+  workspaces: {
+    name: "cdktf-github-actions-terraform-cloud",
+  },
+});
 app.synth();
